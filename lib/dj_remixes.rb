ac0b17db69d556efcdd966b3bd292c409d1d5930
Ben Oakes <ben@benjaminoakes.com>
2011-10-12 14:56:36 -0300

    Edited lib/dj_remixes.rb via GitHub

--- lib/dj_remixes.rb
+++ lib/dj_remixes.rb
@@ -1,4 +1,7 @@
-puts "#{__FILE__} LOADED"
-# path = File.join(File.dirname(__FILE__), 'dj_remixes')
+puts "#{__FILE__} LOADED (without Railties)"
+path = File.join(File.dirname(__FILE__), 'dj_remixes')
 # require File.join(path, 'active_record_railtie')
 # require File.join(path, 'action_mailer', 'action_mailer_railtie')
+require File.join(path, 'worker')
+require File.join(path, 'callbacks')
+require File.join(path, 'airbrake')
